53aa8614665ccc7557231bdb645dc8a9330b1cf8
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-14 13:58:18 -0700

    Create dictConditional.py

---
+++ Windows/Python/dictConditional.py
@@ -0,0 +1,12 @@
+#!python3
+# loop through dictionary keys (error messages) and values (conditionals)
+# if conditional is true, return error message
+
+# create function to search for Boolean values
+def main():
+    error_messages = {
+    "": ""
+        }
+
+if __name__== "__main__":
+    main()
