acb9790094881064994e257ee74dadaf705d352a
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-17 09:25:05 -0700

    Create regex-teser.py

---
+++ Windows/Python/regex-teser.py
@@ -0,0 +1,24 @@
+import re
+
+def regex_test(string):
+    
+    lower_regex = re.compile(r'[a-z]')
+    upper_regex = re.compile(r'[A-Z]')
+    num_regex = re.compile(r'\d')
+    whitespace = re.compile(r'\s')
+    nowhitespace = re.compile(r'\S*')
+    
+    lower_groups = lower_regex.findall(string)
+    upper_groups = upper_regex.findall(string)
+    num_groups = num_regex.findall(string)
+    whitespace_groups = whitespace.findall(string)
+    nowhitespace_groups = nowhitespace.findall(string)
+    print('lowercse: ', lower_groups)
+    print('uppercase: ', upper_groups)
+    print('numbers: ', num_groups)
+    print('whitespace: ', whitespace_groups)
+    print('nowhitespace: ', nowhitespace_groups)
+
+while True:
+    print('This function is for testing regular expressions.\nPlease enter a value.')
+    regex_test(str(input()))
