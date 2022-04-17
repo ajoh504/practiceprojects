6b6132a2909ab9315c238f0697440fcc1f4ccf5b
Alex <83892813+ajoh504@users.noreply.github.com>
2022-04-16 17:45:19 -0700

    Update and rename userInputCD.py to user-input-cd.py
    Completed main() function and added os.chdir()

---
+++ Windows/Python/user-input-cd.py
@@ -0,0 +1,24 @@
+#!python3
+# user-input-cd.py -- Prompt user to change directories
+#
+# create main function to check for valid filepath 
+# change directories if path is valid
+
+import os
+from pathlib import Path
+
+def main(user_input: str) -> str:
+    
+    new_path = Path(user_input)
+    if new_path.is_file() == True:
+        return 'Error: cannot CD to a file!'
+    elif new_path.exists() != True:
+        return 'Error: directory does not exist!'
+    else:
+        os.chdir(new_path)
+        return 'Directory changed to ' + str(new_path)
+
+if __name__ == "__main__":
+    # create print statements
+    print('Use this script to change directories.\nPlease enter a valid Windows file path.')
+    print(main(input()))
